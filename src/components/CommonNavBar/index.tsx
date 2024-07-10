fec37d61a7ec6fb13f046aeca1249b221ca71b5c
hacxy <hacxy.js@outlook.com>
2024-07-10 18:11:02 +0800

    chore: update prettier config

--- src/components/CommonNavBar/index.tsx
+++ src/components/CommonNavBar/index.tsx
@@ -1,15 +1,13 @@
-import React, { memo, useMemo } from "react";
-import { useLocation } from "react-router-dom";
-import { NavBar } from "react-vant";
-import { routeConfig } from "../../routes/config";
+import React, { memo, useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
+import { NavBar } from 'react-vant';
+import { routeConfig } from '../../routes/config';
 
 const CommonNavBar: React.FC = memo(() => {
   const location = useLocation();
 
   const pageName = useMemo(() => {
-    const { name } =
-      routeConfig.find((item) => location.pathname.includes(item.path || "")) ||
-      {};
+    const { name } = routeConfig.find((item) => location.pathname.includes(item.path || '')) || {};
     return name;
   }, [location.pathname]);
 
