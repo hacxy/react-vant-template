6accb3fe77981ab80eb1074d03f5919d60f5017d
hacxy <hacxy.js@outlook.com>
2024-07-11 11:32:06 +0800

    fix: 修复router类型问题

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,18 @@
-import App from "../App.tsx";
-import { createBrowserRouter, redirect } from "react-router-dom";
-import { routeConfig } from "./config.tsx";
+import App from '../App.tsx';
+import { createBrowserRouter, redirect } from 'react-router-dom';
+import { routeConfig } from './config.tsx';
+import type { Router } from '@remix-run/router';
 
-export const router = createBrowserRouter([
+export const router: Router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "/",
-        loader: () => redirect("/home"),
+        path: '/',
+        loader: () => redirect('/home')
       },
-      ...routeConfig,
-    ],
-  },
+      ...routeConfig
+    ]
+  }
 ]);
